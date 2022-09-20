627a28681e7ebc850c4bea5bfb6ba94d28157e69
rz <muhamaul@gmail.com>
2022-09-20 14:20:58 +0700

    [Clojure] updated chapter 12

--- ch12-java/src/ch12_java/core.clj
+++ ch12-java/src/ch12_java/core.clj
@@ -3,7 +3,9 @@
             [clojure.string :as string])
   (:import [com.sun.net.httpserver HttpHandler HttpExchange HttpServer]
            [java.net InetSocketAddress URLDecoder URI]
-           [java.io File FilterOutputStream])
+           [java.io File FilterOutputStream]
+           [java.util Comparator Collections ArrayList]
+           [java.util.concurrent FutureTask])
   (:gen-class))
 
 (defn -main
@@ -131,3 +133,120 @@
         (respond exchange (details file))))))
 
 (update-proxy p {"handle" fs-handler})
+
+(doto (StringBuilder. "abc")
+  (.append (into-array [\x \y \z])))
+;; => #object[java.lang.StringBuilder 0x7c705b3a "abc[Ljava.lang.Character;@7fa629fa"]
+
+(doto (StringBuilder. "abc")
+  (.append (char-array [\x \y \z])))
+;; => #object[java.lang.StringBuilder 0x6da19eb3 "abcxyz"]
+
+(let [ary (make-array Long/TYPE 3 3)]
+  (dotimes [i 3]
+    (dotimes [j 3]
+      (aset ary i j (+ i j))))
+  (map seq ary))
+;; => ((0 1 2) (1 2 3) (2 3 4))
+
+(into-array Integer/TYPE [1 2 3])
+;; => #object["[I" 0x5f8e6ce1 "[I@5f8e6ce1"]
+
+(def ary (into-array [1 2 3]))
+;; => #'ch12-java.core/ary
+(def sary (seq ary))
+;; => #'ch12-java.core/sary
+sary
+;; => (1 2 3)
+
+(aset ary 0 42)
+;; => 42
+
+sary
+;; => (42 2 3)
+
+(defn asum-sq
+  [xs]
+  (let [db1 (amap xs i ret
+                  (* (aget xs i)
+                     (aget xs i)))]
+    (areduce db1 i ret 0
+             (+ ret (aget db1 i)))))
+;; => #'ch12-java.core/asum-sq
+
+(defmulti what-is class)
+;; => #'ch12-java.core/what-is
+(defmethod what-is
+  (Class/forName "[Ljava.lang.String;")
+  [_]
+  "1d String")
+;; => #multifn[what-is 0x440212f4]
+
+(defmethod what-is
+  (Class/forName "[[Ljava.lang.Object;")
+  [_]
+  "2d Object")
+;; => #multifn[what-is 0x440212f4]
+
+(defmethod what-is
+  (Class/forName "[[[[I")
+  [_]
+  "Primitive 4d int")
+;; => #multifn[what-is 0x440212f4]
+
+(defmethod what-is
+  (Class/forName "[[D")
+  [a]
+  "Primitive 2d double")
+;; => #multifn[what-is 0x440212f4]
+
+(defmethod what-is
+  (Class/forName
+   "[Lclojure.lang.PersistentVector;")
+  [a]
+  "1d Persistent Vector")
+;; => #multifn[what-is 0x440212f4]
+
+(asum-sq (double-array [1 2 3 4 5]))
+;; => 55.0
+
+(what-is (into-array ["a" "b"]))
+;; => "1d String"
+;; => 
+
+(what-is (to-array-2d [[1 2] [3 4]]))
+;; => "2d Object"
+
+(what-is (make-array Integer/TYPE 2 2 2 2))
+;; => "Primitive 4d int"
+
+(what-is (into-array (map double-array [[1.0] [2.0]])))
+;; => "Primitive 2d double"
+
+(what-is (into-array [[1.0] [2.0]]))
+;; => "1d Persistent Vector"
+
+(String/format "An int %d and a String %s"
+               (to-array [99, "luftballons"]))
+;; => "An int 99 and a String luftballons"
+
+(defn gimme
+  []
+  (ArrayList. [1 3 4 8 2]))
+;; => #'ch12-java.core/gimme
+
+(doto (gimme)
+  (Collections/sort (Collections/reverseOrder)))
+;; => [8 4 3 2 1]
+
+(doto (Thread. #(do (Thread/sleep 5000)
+                    (println "hehehee")))
+  .start)
+;; => #object[java.lang.Thread 0x346f4c84 "Thread[Thread-35,5,main]"]
+;; ... 5 s later
+;; => hehehee
+
+(let [f (FutureTask. #(do (Thread/sleep 5000) 42))]
+  (.start (Thread. #(.run f)))
+  (.get f))
+;; => 42
